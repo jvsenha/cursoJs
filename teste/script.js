0d7d9a039f6857aaf90123d6c7c5904c1165d888
jvsenha <joao.santos1381@etec.sp.gov.br>
2024-03-22 15:02:51 -0300

    exercicio_ps 1

--- teste/script.js
+++ teste/script.js
@@ -1,10 +1,67 @@
-function verifica() { 
-    var inp1= document.getElementById('inp1')
-    var inp2= document.getElementById('inp2')
-    var btn = document.getElementById('btn1')
-    if (inp1.value != '' && inp2.value != '' ) {
-        btn.style.visibility='visible'; return; } 
- else if(inp1.value == '' || inp2.value == '' ){
-    btn.style.visibility='hidden'; 
- }
-}
+let pedido1 = {
+    numPedido: "0001",
+    dataPedido: "2024-03-22",
+    itens: [
+        {
+            nome: "Camisa",
+            quantidade: 2,
+            precoUnitario: 25.99,
+        },
+        {
+            nome: "Calça Jeans",
+            quantidade: 1,
+            precoUnitario: 39.99,
+        }],
+    cliente: {
+        nome: "João Silva",
+        endereco: "Rua das Flores, 123",
+        cidade: "São Paulo"
+    },
+    status: "Em processamento"
+};
+
+let pedido2 = {
+    numPedido: "0002",
+    dataPedido: "2024-03-21",
+    itens: [
+        {
+            nome: "Tênis",
+            quantidade: 1,
+            precoUnitario: 59.99,
+        },
+        {
+            nome: "Meias",
+            quantidade: 3,
+            precoUnitario: 5.99,
+        }],
+    cliente: {
+        nome: "Maria Souza",
+        endereco: "Av. Principal, 456",
+        cidade: "Rio de Janeiro"
+    },
+    status: "Aguardando pagamento"
+};
+
+let pedido3 = {
+    numPedido: "0003",
+    dataPedido: "2024-03-20",
+    itens: [
+        {
+            nome: "Celular",
+            quantidade: 1,
+            precoUnitario: 699.99,
+        },
+        {
+            nome: "Fone de Ouvido",
+            quantidade: 1,
+            precoUnitario: 29.99,
+        }],
+    cliente: {
+        nome: "Carlos Oliveira",
+        endereco: "Travessa das Palmeiras, 789",
+        cidade: "Belo Horizonte"
+    },
+    status: "Enviado"
+};
+
+let listaPedidos=[pedido1,pedido2,pedido3]
